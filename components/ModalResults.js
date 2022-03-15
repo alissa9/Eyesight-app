c3c26f0cd09c494a69aa506b192533b2c4dce9f4
Ikbal Alissa <M99alissa@gmail.com>
2022-03-15 18:30:53 +0000

    added picker to pick differnet languages voices

--- components/ModalResults.js
+++ components/ModalResults.js
@@ -12,7 +12,7 @@ import {
 import tw from "tailwind-rn";
 import * as Speech from "expo-speech";
 import { AntDesign } from "@expo/vector-icons";
-
+import { Picker } from "@react-native-picker/picker";
 
 const ModalResults = ({ showModal, objectLables }) => {
   const [modalVisible, setModalVisible] = useState(false);
@@ -24,6 +24,7 @@ const ModalResults = ({ showModal, objectLables }) => {
     quality: "Default",
   });
 
+  const [country, setCountry] = useState("");
   // Gets a list of all the available languages and voices
   useEffect(() => {
     (async () => {
@@ -59,29 +60,38 @@ const ModalResults = ({ showModal, objectLables }) => {
         }}
       >
         <View style={tw("flex-1 justify-center items-center  ")}>
-          <View
-            style={tw(
-              " bg-blue-100 rounded-2xl items-center mb-24 p-8 bg-blue-200"
-            )}
-          >
-            <View style={tw("flex flex-row flex-wrap ")}>
-              {voices.map((voice) => (
-                <TouchableOpacity
-                  style={tw("m-3 ")}
-                  onPress={() => textToSpeech(voice)}
-                >
-                  <Text> {voice.language}</Text>
-                </TouchableOpacity>
-              ))}
+          <View style={tw("bg-blue-200 rounded-2xl items-center mb-24 p-8 ")}>
+            <View style={tw("flex flex-row flex-wrap mb-12")}>
+              <Picker
+                itemSyle={{
+                  fontSize: 10,
+                  color: "black",
+                  textAlign: "center",
+                  fontWeight: "bold",
+                }}
+                style={{ width: 300 }}
+                selectedValue={country}
+                onValueChange={(val, index) => {
+                  setCountry(val);
+                  textToSpeech(val);
+                }}
+                mode="dropdown"
+              >
+                <Picker.Item label="select your Language" value="Unknown" />
+
+                {voices.map((voice) => (
+                  <Picker.Item label={voice.language} value={voice} />
+                ))}
+              </Picker>
             </View>
             {objectLables &&
               objectLables.map((label, index) => (
-                <Text style={tw("font-extrabold  text-xl  ")} key={index}>
+                <Text style={tw("font-extrabold  text-xl mt-3  ")} key={index}>
                   {label.description}
                 </Text>
               ))}
             <Pressable
-              style={tw("rounded-xl  bg-gray-400 mt-4 p-5  ")}
+              style={tw("rounded-xl  bg-gray-400 mt-24 p-5  ")}
               onPress={() => setModalVisible(!modalVisible)}
             >
               <Text style={tw("text-black font-bold")}>Hide Results</Text>
